489357954dcce31afec991c29a1167ad8ab61ce5
Glen Peek <glenpeek@PsukheDelos-2.local>
2016-01-30 00:05:52 +1300

    remove all files

--- _config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package silverstripe-versionedfiles
- */
-
-File::add_extension('VersionedFileExtension');
-Image::add_extension('VersionedImageExtension');
-Folder::add_extension('VersionedFolderExtension');
-Upload::add_extension('VersionedUploadExtension');
